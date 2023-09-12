c393095282bf4219e9d340edb5bf92c4e62f513e
Lukáš Huvar <137092577+LukasRaider@users.noreply.github.com>
2023-09-12 08:54:25 +0200

    Add files via upload

---
+++ src/dogsData.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+export const dogs = [
+  {
+    id: 1,
+    name: "Jackers",
+    race: "Labrador",
+    age: 4,
+  },
+  {
+    id: 2,
+    name: "Forty",
+    race: "Jezevčík",
+    age: 6,
+  },
+  {
+    id: 3,
+    name: "Amy",
+    race: "Německý ovčák",
+    age: 8,
+  },
+  {
+    id: 4,
+    name: "Bibi",
+    race: "Kokršpaněl",
+    age: 4,
+  },
+  {
+    id: 5,
+    name: "Bulik",
+    race: "bullteriér",
+    age: 6,
+  },
+];
